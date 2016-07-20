0026e5aa3f3db690d40d04b7d1df8f8a6d11ab3d
mfulford <maxwell-fulford@hotmail.com>
2016-07-20 18:43:01 +0100

    Front End registration edit, bcc receipts, navbar dropdown on mobile

--- app/mailers/charges_mailer.rb
+++ app/mailers/charges_mailer.rb
@@ -1,9 +1,9 @@
 class ChargesMailer < ActionMailer::Base
-  default from: "receipts@instela.co"
+  default from: "receipts@instela.co", return_path: 'help@instela.co'
 
   def send_receipt(charge)
     @charge = charge
-    mail to: @charge.account.email, subject: "Receipt for Order ##{charge.id}"
+    mail to: @charge.account.email, subject: "Receipt for Order ##{charge.id}",  bcc: ["help@instela.co"]
   end
 
 
